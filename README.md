e4b0f61af90522f5e28fa52ca021d071ba49e69a
Yannick Bonnefond <yannick.bonnefond@gmail.com>
2017-04-06 16:37:47 +0200

    init project

---
+++ README.md
@@ -0,0 +1,24 @@
+# Sample Bridge V3 API PHP client
+
+## Prerequisites
+
+```bash
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+```
+
+## Installation
+
+```bash
+php composer.phar install
+```
+
+## Variables
+Replace variables in index.php with the one provided by Leadformance staff.
+
+
+## Run
+
+```bash
+php src/index.php
+```
---
+++ src/index.php
@@ -0,0 +1,83 @@
+<?php
+namespace Bridge;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Bridge\Auth\AuthProvider;
+use GuzzleHttp\Client;
+
+// Config
+$clientId = '<INSERT CLIENT ID HERE>';
+$secret = '<INSERT CLIENT SECRET HERE>';
+$tokenEndpoint = '<INSERT SSO TOKEN ENDPOINT HERE>';
+$bridgeApiUrl = '<INSERT BRIDGE API URL HERE>';
+
+// Retrieve access token
+$provider = new AuthProvider($clientId, $secret, $tokenEndpoint);
+$authInfo = $provider->getToken();
+expiresIn($authInfo);
+
+// Create Client
+$api = new Client([
+    'base_uri' => $bridgeApiUrl,
+    'headers' => [
+        'Authorization' => 'Bearer ' . $authInfo['access_token'],
+        'Accept' => 'application/vnd.bridge-v1+json'
+    ],
+]);
+
+// List locations
+$list = decodeResponse($api->get("/locations"));
+println("Total: {$list['total']} locations");
+
+// Create a new Location
+$data = [
+    'name' => 'Leadformance',
+    'localisation' => [
+        'address1' => '19 Rue du Lac Saint-André',
+        'city' => 'Le Bourget-du-Lac',
+        'postalCode' => '73370',
+        'countryCode' => 'FR'
+    ]
+];
+$createdLocation = decodeResponse($api->post('/locations', [
+    'json' => $data
+]));
+println("New location created | ID: {$createdLocation['_id']}");
+
+// Get the created location
+$location = decodeResponse($api->get("/locations/{$createdLocation['_id']}"));
+println("Locationretrieved | ID: {$location['_id']}");
+
+// // Update a location
+println("Adding website to location {$location['_id']}");
+$location['website'] = 'https://www.leadformance.com';
+
+// Update a Location
+$updatedLocation = decodeResponse($api->put("/locations/{$location['_id']}", $location));
+println("Location updated, new website is {$updatedLocation['website']}");
+
+
+function decodeResponse($response) {
+    $status = $response->getStatusCode();
+    $body = (string)$response->getBody();
+    if (in_array($status, [200,201])) {
+        return json_decode($body, true);
+    } else {
+        println("Error response received from API with status $status");
+        var_dump($body);
+        exit(1);
+    }
+}
+
+function println($line) {
+    echo "$line\n";
+}
+
+function expiresIn($info) {
+    $d1 = new \DateTime();
+    $d2 = new \DateTime();
+    $d2->add(new \DateInterval('PT'.$info['expires_in'].'S'));
+
+    println($d2->diff($d1)->format("Token expires in %a days, %h hours, %i minutes, %s seconds"));
+}
